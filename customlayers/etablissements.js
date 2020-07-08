db9e91f08ad096c7b89031c56832c20a1d5c6ccd
Gaetanbrl <gaetan.bruel@jdev.fr>
2020-07-08 10:44:35 +0200

    Display cluster data and remove queryable config

--- customlayers/etablissements.js
+++ customlayers/etablissements.js
@@ -1,17 +1,61 @@
 mviewer.customLayers.etablissements = (function () {
     var id = 'etablissements';
     var data = 'apps/cartoHoraires/data/etablissements.geojson'
-   
-    var vectorSource = new ol.source.Vector({
-        format: new ol.format.GeoJSON(),
-        url : data
-    });
+    
+    function manyStyle (radius, size, color) {
+      return [
+        new ol.style.Style({
+          image: new ol.style.Circle({
+              radius: radius,
+              fill: new ol.style.Fill({
+                  color: color
+              }),
+              stroke: new ol.style.Stroke({
+                width: 1.8,
+                color: 'white'
+              })
+          })
+        })
+      ];
+    };
+
+    function pointStyle (color) {
+      var style =  new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 3,
+          fill: new ol.style.Fill({color: color}),
+          stroke: new ol.style.Stroke({color: 'black', width: 1})
+        })
+      });
+      return [style];
+    }
+
+    function clusterStyle (feature) {
+      var size = feature.get('features').length;
+      var max_radius = 30;
+      var max_value = 500;
+      var radius = 10 + Math.sqrt(size)*(max_radius / Math.sqrt(max_value));
+      var color = '#53B3B8';
+
+      if(size > 1) {
+        return manyStyle(radius, size, color);
+      } else {
+        return pointStyle(color);
+      }
+    }
 
     var vectorLayer = new ol.layer.Vector({
-        source: vectorSource
-      });
+      source: new ol.source.Cluster({
+          distance: 0,
+          source: new ol.source.Vector({
+              url: data,
+              format: new ol.format.GeoJSON()
+          })
+      }),
+      style: clusterStyle
+    });
 
     return {
-      layer: vectorLayer
+        layer: vectorLayer
     }
   }());
