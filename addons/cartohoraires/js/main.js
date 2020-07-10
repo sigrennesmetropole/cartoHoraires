e4728ea12fde02183e5b150266535cd298c3ca30
Gaetanbrl <gaetan.bruel@jdev.fr>
2020-07-10 13:17:47 +0200

    Add SIRENE autocomplete with zoom on map behavior

--- addons/cartohoraires/js/main.js
+++ addons/cartohoraires/js/main.js
@@ -3,6 +3,13 @@ const cartohoraires = (function() {
     let options = mviewer.customComponents.cartohoraires.config.options;
     const mapWidth = options.mapWidth;
     const itemsRight = options.templateWidth + 2;
+
+    let selected = null;
+
+    /**
+     * PRIVATE
+    */
+
     /**
      * Standardize request creation
      * @param {Object} infos as data params
@@ -10,9 +17,9 @@ const cartohoraires = (function() {
      */
     const createRequest = function(infos, successFunc) {
         return {
-            url:infos.url,
+            url: infos.url,
             data: infos.data,
-            success: function (r) {
+            success: function(r) {
                 infos.success(r);
             }
         };
@@ -20,8 +27,8 @@ const cartohoraires = (function() {
 
     /**
      * Request to call Mustache template from server or local path.
-    */
-    const initTemplate = function () {
+     */
+    const initTemplate = function() {
         let req = createRequest({
             url: options.template,
             success: function(template) {
@@ -31,16 +38,16 @@ const cartohoraires = (function() {
         });
         $.ajax(req);
     }
-        /**
+    /**
      * Method from Mviewer from features to template.
      * Manage display / hide and content from mustache template.
      * @param {Array} features - Array of OpenLayers features
      * @param {Object} configuration - Mviewer configuration
      */
-    function displayTemplate (template) {
+    function displayTemplate(template) {
         // render mustache file
         var panelContent = Mustache.render(template);
-        if(configuration.getConfiguration().mobile) {
+        if (configuration.getConfiguration().mobile) {
             // hide classic panel
             $('.cartohoraires-panel').hide();
             // clean modal content
@@ -49,7 +56,9 @@ const cartohoraires = (function() {
             $("#cartohoraires-modal .modal-body").append(panelContent);
         } else {
             // close modal if visible
-            if($("#cartohoraires-modal").is(':visible')){$("#cartohoraires-modal").modal('toggle');}
+            if ($("#cartohoraires-modal").is(':visible')) {
+                $("#cartohoraires-modal").modal('toggle');
+            }
             // clean classic panel
             $(".cartohoraires-panel .panel-body").children().remove();
             // add to classic panel
@@ -57,33 +66,99 @@ const cartohoraires = (function() {
         }
     }
 
+    /**
+     * Display modal on mobile device only and display it only if a user clic on button
+     */
     function initModalBehavior() {
-        mviewer.getMap().once('postrender', m => {
-            $('#mobilebtn-el').on('click', function() {
-                $("#cartohoraires-modal").modal('toggle');
+        $('#mobilebtn-el').on('click', function() {
+            $("#cartohoraires-modal").modal('toggle');
+        });
+    }
+
+    /**
+     * Force display component for desktop device only
+     */
+    function initDisplayComponents() {
+        if (!configuration.getConfiguration().mobile) {
+            $('#map').attr('style', `width:${mapWidth}% !important`);
+            $('#zoomtoolbar').attr('style', `right: ${itemsRight}% !important`);
+            $('#toolstoolbar').attr('style', `right: ${itemsRight}% !important`);
+        }
+    }
+
+    function initSearchItem() {
+        mviewer.getMap().on('postrender', m => {
+            $('.search-input').autoComplete({
+                resolver: 'custom',
+                minLength: 3,
+                resolverSettings: {
+                    requestThrottling: 50
+                },
+                formatResult: function (record) {
+                    let txt = [record.fields.denominationunitelegale, record.fields.libellecommuneetablissement].join(', ');
+                    let rec = JSON.stringify(record);
+                    let str = `<div style='overflow-x:hidden;'>
+                                    <a href="#" onclick='cartohoraires.searchBehavior(${rec})'>${txt}</a>
+                                </div>`;
+                    return {
+                        value: record.datasetid,
+                        text: txt,
+                        html: [str]
+                    };
+                },
+                noResultsText: '<div style="overflow-x:hidden"><span>Aucun résultat - Merci de sélectionner une adresse<span></div>',
+                events: {
+                    search: function (qry, callback) {
+                        // let's do a custom ajax call
+                        $.ajax(
+                            'https://data.rennesmetropole.fr/api/records/1.0/search/',
+                            {
+                                data: {
+                                'dataset':'base-sirene-v3-ss',
+                                'q' : `denominationunitelegale = ${qry}`,
+                                'rows': 5
+                                }
+                            }
+                        ).done(function (res) {
+                            callback(res.records);
+                        });
+                    }
+                }
             });
         });
+        $('.bootstrap-autocomplete.dropdown-menu').css('width','100%');
     }
 
-    function initDisplayComponents() {
-        mviewer.getMap().once('postrender', m => {
-            if(!configuration.getConfiguration().mobile) {
-                $('#map').attr('style',`width:${mapWidth}% !important`);
-                $('#zoomtoolbar').attr('style',`right: ${itemsRight}% !important`);
-                $('#toolstoolbar').attr('style',`right: ${itemsRight}% !important`);
-            }
-        });
-
+    function displayResult() {
+        if(selected) {
+           var geom = selected.geometry.coordinates;
+           mviewer.zoomToLocation(geom[0], geom[1], 16, null);
+        }
     }
+
+    function setSelected(e) {return selected = e;}
+
+    /**
+     * PUBLIC
+    */
 
     return {
-        init : () => {
-            console.log('init cartohoraires module');
-            initDisplayComponents();
-            initTemplate();
-            initModalBehavior();            
+        init: () => {
+            mviewer.getMap().once('postrender', m => {
+                initTemplate();
+                initDisplayComponents();
+                initModalBehavior();
+                
+            });
+            initSearchItem();
         },
+        searchBehavior : function(e) {
+            if(e) {
+                setSelected(e);
+                displayResult();
+            }
+        }
     };
 })();
 
-new CustomComponent("cartohoraires", cartohoraires.init)
+new CustomComponent("cartohoraires", cartohoraires.init);
